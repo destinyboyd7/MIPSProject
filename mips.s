e8f3952c38a9127eed533313281a462b9e4137c9
destinyboyd7 <124649827+destinyboyd7@users.noreply.github.com>
2024-02-15 15:49:52 -0500

    user input
    prompt user input and read it.

--- mips.s
+++ mips.s
@@ -1,1 +1,20 @@
 .data
+prompt:         .asciiz "Enter a string: "
+input_string:   .space 1001   #buffer
+newline:        .asciiz " / "
+dash:           .asciiz "-"
+
+.text
+main:
+  # Prompt user for input
+    li $v0, 4
+    la $a0, prompt
+    syscall
+
+    # Read user input into input_string
+    li $v0, 8
+    la $a0, input_string
+    li $a1, 1000
+    syscall
+
+  
