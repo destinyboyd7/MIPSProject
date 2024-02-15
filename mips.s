136c65b0ec7cc8af2057c46df36be2baebf4726c
destinyboyd7 <124649827+destinyboyd7@users.noreply.github.com>
2024-02-15 16:52:40 -0500

    check_digit
    loaded substring into process_substring and created a loop for substring. check_digit checks the ascii value of character to see if it is a digit.

--- mips.s
+++ mips.s
@@ -32,7 +32,7 @@ process_whole_string:
         lb $t2, 0($a0)
         beq $t2, $0, end_whole_string
 
-        bne $t2, 47, slash_found #checks if character is a forward slash
+        beq $t2, 47, slash_found #checks if character is a forward slash
 
         sb $t2, 0($s0)  #store the character into string beuffer
         sub $sp, $sp, 4 #make space is stack
@@ -45,7 +45,7 @@ process_whole_string:
 
         j print_sum
 
-        found_slash:
+        slash_found:
           addi $a0, $a0, 1  #move to the next character in the input string 
           j whole_string_loop
           
@@ -71,11 +71,25 @@ process_substring:
     
     substring_loop:
       lb $t7, 0($t6) # load 
-      beqz $t7, print_result   # If null terminator is encountered, print the result
+      beq $t7, $0, print_result   # If null terminator is encountered, print the result
     
+      check_digit: 
+        blt $t7, $t1, not_valid  # If the character is not a digit, go to not_valid
+        bgt $t7, $t0, not_digit
 
-      blt $t7, $t1, not_valid  # If the character is not a digit, go to not_valid
-      bgt $t7, $t0, not_digit
+        # Convert ASCII digit to integer and update sum
+        sub $t7, $t7, $t1
+        add $t8, $t8, $t7
+
+        # Move to the next character in the input string
+        addi $t6, $t6, 1
+        j substring_loop
+
+      
+
+      
+      
+
     
 
         
