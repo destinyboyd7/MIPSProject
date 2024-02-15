5845684f8a8bcd91a059925c47b80b01f8bf4a6b
destinyboyd7 <124649827+destinyboyd7@users.noreply.github.com>
2024-02-15 16:23:57 -0500

    stack
    trying to store value into stack so it can be passed

--- mips.s
+++ mips.s
@@ -1,8 +1,9 @@
 .data
 prompt:         .asciiz "Enter a string: "
 input_string:   .space 1001   #buffer
-newline:        .asciiz " / "
+space_slash:        .asciiz " / "
 dash:           .asciiz "-"
+
 
 .text
 main:
@@ -25,12 +26,33 @@ main:
      syscall
 
 
-process_whole_strinng:
-     move $t1, $a0
-
+process_whole_string:
+    li $t8, 0     #index for substring 
     whole_string_loop: 
-        lb $t2, 0($t1)
+        lb $t2, 0($a0)
         beq $t2, $0, end_whole_string
+
+        bne $t2, 47, slash_found #checks if character is a forward slash
+
+        sb $t2, 0(t8)  #store the character into string beuffer
+        sub $sp, $sp, 4 
+        sw $a0, 0($sp)
+
+        jal process_substring
+
+        lw $a0, 0($sp) 
+        add $sp, $sp, -4
+        
+        addi $t2, $t2, 1
+        addi $t8, $t8, 1
+
+        found_slash:
+          addi $a0, $a0, 1  #move to the next character in the input string 
+          j whole_string_loop
+
+    end:
+        no_slash
+        j next_char
 
         
      
